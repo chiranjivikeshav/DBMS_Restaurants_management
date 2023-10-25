9d7f6c69641d427aece9f258f660da298fc9f7fc
chiranjivi <chiranjivikeshav1@gmail.com>
2023-10-25 22:24:10 +0530

    Initial commit

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,10 @@
-
+from django.contrib import admin
 from django.urls import path
-
+from restaurant import views
 urlpatterns = [
-    
+   path('',views.home,name='home'), 
+   path('authpage',views.authpage,name='authpage'),
+   path('user_registration',views.user_registration,name='user_registration'),
 ]
+
+    
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . import urls
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def authpage(request):
+    return render(request,"auth.html")
+def user_registration(request): 
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        useremail = request.POST['user_email']
+        password = request.POST['user_password']
+        existing_user = User.objects.filter(username=username).exists()
+        if existing_user:
+            messages.warning(request,'Username already exists')
+            return render(request,"auth.html")
+        existing_EMAIL = User.objects.filter(email=useremail).exists()
+        if existing_EMAIL:
+            messages.warning(request,'Email Address already exists')
+            return render(request,"auth.html")
+        user = User.objects.create_user(username=username,email=useremail, password=password)
+        messages.success(request, 'Your account has been created successfully!')
+        return render(request,"auth.html")
+    return render(request,'home.html') 
