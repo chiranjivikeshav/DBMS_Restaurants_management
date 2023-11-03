694587c078ab826dc18316c5a3c3f349da600c22
chiranjivi <chiranjivikeshav1@gmail.com>
2023-11-03 13:48:20 +0530

    restaurant manage

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gcb-omnrs79%1w2m_5!ws^n^u43uqc@q#sch5gz45badizk#^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
