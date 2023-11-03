baae90cc45fc9c7dec930d91006a58c347318456
chiranjivi <chiranjivikeshav1@gmail.com>
2023-11-03 16:05:57 +0530

    restaurant manage

--- server/settings.py
+++ server/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
-MEDIA_URL="/media/"
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
